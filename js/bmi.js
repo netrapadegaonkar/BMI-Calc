3876b8716d0266f27f11253b6f7f1fc6da595906
netrapadegaonkar <netrapadegaonkar@gmail.com>
2014-04-20 16:01:13 -0700

    change

--- js/bmi.js
+++ js/bmi.js
@@ -13,6 +13,8 @@ $('#under2').hide();
 $('#bmiresult').hide();
 $('#overweightc').hide();
 $('#overweightl').hide();
+$('#overp').hide();
+$('#underp').hide();
 
 
 }
@@ -24,7 +26,7 @@ function calc(){
 	$('#bmiresult').show();
 	$('#text').hide();
 	$('#ans').show();
-	$('#bmi').html("BMI &raquo; "+Math.round(d * 100) / 100);
+	$('#bmi').html("BMI: &nbsp; &nbsp; "+Math.round(d * 100) / 100);
 	if(d < 18.5){
 $('#status').html("You are UNDERWEIGHT");
 $('#under').show();
@@ -57,17 +59,7 @@ var q=document.forms["f1"]["height"].value;
 						
 						
 	
-/*function validate(evt) {
-  var theEvent = evt || window.event;
-  var key = theEvent.keyCode || theEvent.which;
-  key = String.fromCharCode( key );
-  var regex = /[0-9]|\./;
-  if( !regex.test(key) ) {
-    theEvent.returnValue = false;
-    if(theEvent.preventDefault) theEvent.preventDefault();
-  }  
- }*/
-  
+
 function reset(){
 	document.f1.weight.value="";
 	document.f1.height.value="";
@@ -77,22 +69,38 @@ function reset(){
 	{
 		$('#under2').hide();
 		$('#under1').show();
+		$('#underp').hide();
 	}
 	
 	function underweight1()
 	{
 		$('#under1').hide();
 		$('#under2').show();
+		$('#underp').hide();
+	}
+	
+	function underweight2()
+	{
+		$('#under1').hide();
+		$('#under2').hide();
+		$('#underp').show();
 	}
 	
 	function overweightc(){
 		$('#overweightc').show();
 		$('#overweightl').hide();
+		$('#overp').hide();
 		
 	}
 	
 	function overweightl(){
 		$('#overweightc').hide();
 		$('#overweightl').show();
-		
+		$('#overp').hide();
+	}
+
+	function overweight2(){
+		$('#overweightc').hide();
+		$('#overweightl').hide();
+		$('#overp').show();
 	}
